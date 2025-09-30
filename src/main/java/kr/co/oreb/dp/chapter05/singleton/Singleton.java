459ed6d29aa7d68ae203e86e57b1f49575ce7f5b
ore-b <turtle.oreb@gmail.com>
2025-10-01 02:16:27 +0900

    singleton

---
+++ src/main/java/kr/co/oreb/dp/chapter05/singleton/Singleton.java
@@ -0,0 +1,23 @@
+package kr.co.oreb.dp.chapter05.singleton;
+
+public class Singleton {
+
+    private volatile static Singleton instance;
+
+    private Singleton() {
+    }
+
+    public static Singleton getInstance() {
+
+        if (instance == null) {
+            synchronized (Singleton.class) {
+                if (instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+
+        return instance;
+    }
+
+}
